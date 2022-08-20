a2395383c90b515796890e3ec375182566507c9c
Ignacio Buioli <ibuioli@gmail.com>
2022-08-20 17:59:53 -0300

    feat: NG13

--- maquinaGirondoJS/src/polyfills.ts
+++ maquinaGirondoJS/src/polyfills.ts
@@ -19,19 +19,8 @@
 */
 
 
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
-
-/** IE10 and IE11 requires the following to support `@angular/animation`. */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
-
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
-
-
-/** ALL Firefox browsers require the following to support `@angular/animation`. **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
 
